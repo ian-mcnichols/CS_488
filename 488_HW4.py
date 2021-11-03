4ba5d918d2c5c0112d4ed231efb3898d35d10233
Ian McNichols <90727326+ian-mcnichols@users.noreply.github.com>
2021-11-03 16:39:39 -0500

    Add files via upload

--- 488_HW4.py
+++ 488_HW4.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import plot_confusion_matrix, ConfusionMatrixDisplay
 from sklearn.pipeline import Pipeline
 
 
@@ -193,6 +193,7 @@ def iris_classification(run_pca=False, run_lda=True):
         X = lda.fit(X, y).transform(X)
 
     test_sizes = [.9, .8, .7, .6, .5]
+    confusion_matrices = []
     for test_size in test_sizes:
         # Split dataset based on test size
         X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=test_size,random_state=1,shuffle=True)
@@ -208,14 +209,9 @@ def iris_classification(run_pca=False, run_lda=True):
             # Fit to the training dataset
             pipeline.fit(X_train, Y_train)
             # Create a confusion matrix
-            disp = plot_confusion_matrix(pipeline, X_validation, Y_validation, cmap=plt.cm.Blues)
-            true_positive = disp.confusion_matrix[1][1]
-            false_negative = disp.confusion_matrix[1][0]
-            true_negative = disp.confusion_matrix[0][0]
-            false_positive = disp.confusion_matrix[0][1]
-            # Calculate sensitivity and specificity from confusion matrix
-            sensitivity = true_positive / (true_positive + false_negative)
-            specificity = true_negative / (true_negative + false_positive)
+            if test_size == .7:
+                disp = plot_confusion_matrix(pipeline, X_validation, Y_validation, cmap=plt.cm.Blues)
+                confusion_matrices.append(disp.confusion_matrix)
             # Calculate and store overall accuracy of the model
             classification_score = pipeline.score(X_validation, Y_validation)
             classification_accuracy.append(classification_score*100)
@@ -225,10 +221,16 @@ def iris_classification(run_pca=False, run_lda=True):
             data_line = "test size: " + str(test_size) + " model: " + str(name) + " training accuracy: " +\
                         str(pipeline.score(X_train, Y_train)*100) + " classification accuracy: " +\
                         str(classification_score*100)
-            data_line += " sensitivity: " + str(sensitivity) + " specificity: " + str(specificity) + "\n"
             print(data_line)
         # Combine all the averages per test size
         plot_data[test_size] = [training_accuracy, classification_accuracy]
+    confusion_matrices = np.array(confusion_matrices)
+    avg_matrix = np.mean(confusion_matrices, axis=0)
+    print("Confusion matrix:", avg_matrix)
+    plt.figure()
+    display_matrix = ConfusionMatrixDisplay(avg_matrix)
+    display_matrix.plot()
+    plt.show()
     # Display average overall accuracy and training accuracy to output for each test size
     training_accuracies = [plot_data[x][0] for x in plot_data]
     print("Training accuracies:", training_accuracies)
@@ -249,7 +251,18 @@ def pines_classification(R_filepath, run_pca=False, run_lda=False):
     R_cols = R_file['num_cols']
     R_bands = R_file['num_bands']
     gth = np.reshape(gth, (int(R_rows)*int(R_cols)))
-
+    # Ugly but works. Remove all the data where there isn't a ground truth
+    zeros = (gth == 0).nonzero()
+    print(gth.shape)
+    print(X.shape)
+    print("zeros:", zeros)
+    for i in range(len(zeros)):
+        zeros_new = (gth == 0).nonzero()
+        delete_index = zeros_new[0]
+        gth = np.delete(gth, delete_index)
+        X = np.delete(X, delete_index, axis=0)
+    print(gth.shape)
+    print(X.shape)
     if run_pca:
         # Run PCA on data and save back to the X variable
         pca = PCA(n_components=10)
@@ -272,8 +285,11 @@ def pines_classification(R_filepath, run_pca=False, run_lda=False):
         print("X:", X.shape)
         print("gth:", gth.shape)
         print("Running without dimensionality reduction")
+    print("GTH max class:", gth.max())
     plot_data = {}
     test_sizes = [.9, .8, .7, .6, .5]
+    test_sizes = [.7]
+    confusion_matrices = []
     for test_size in test_sizes:
         # Splitting data by test size
         X_train, X_validation, Y_train, Y_validation = train_test_split(X, gth, test_size=test_size,
@@ -288,15 +304,10 @@ def pines_classification(R_filepath, run_pca=False, run_lda=False):
             pipeline = Pipeline([("scaler", MinMaxScaler()), ("classifier", model)])
             # Fit to the training dataset
             pipeline.fit(X_train, Y_train)
-            # Make a confusion matrix
-            disp = plot_confusion_matrix(pipeline, X_validation, Y_validation, cmap=plt.cm.Blues)
-            # Grab values for sensitivity/specificty
-            true_positive = disp.confusion_matrix[1][1]
-            false_negative = disp.confusion_matrix[1][0]
-            true_negative = disp.confusion_matrix[0][0]
-            false_positive = disp.confusion_matrix[0][1]
-            sensitivity = true_positive / (true_positive + false_negative)
-            specificity = true_negative / (true_negative + false_positive)
+            if test_size == .7:
+                # Make a confusion matrix
+                disp = plot_confusion_matrix(pipeline, X_validation, Y_validation, cmap=plt.cm.Blues)
+                confusion_matrices.append(disp.confusion_matrix)
             # Overall model classification score vs training data score
             classification_score = pipeline.score(X_validation, Y_validation)
             train_score = pipeline.score(X_train, Y_train)
@@ -305,11 +316,17 @@ def pines_classification(R_filepath, run_pca=False, run_lda=False):
             classification_accuracy.append(classification_score*100)
             # Printing model information to output
             data_line = "test size: " + str(test_size) + " model: " + str(name) + " training accuracy: " + str(train_score*100) + " classification accuracy: " + str(classification_score*100)
-            data_line += " sensitivity: " + str(sensitivity) + " specificity: " + str(specificity) + "\n"
             print(data_line)
         # Adding all model averages to a dictionary corresponding with test size
-        # TODO: Fix it from being averages
         plot_data[test_size] = [training_accuracy, classification_accuracy]
+    # Average out all the display matrices and plot it
+    confusion_matrices = np.array(confusion_matrices)
+    avg_matrix = np.mean(confusion_matrices, axis=0)
+    print("Confusion matrix:", avg_matrix)
+    plt.figure()
+    display_matrix = ConfusionMatrixDisplay(avg_matrix)
+    display_matrix.plot()
+    plt.show()
     # Displaying overall accuracies for each test size
     test_sizes = [x*100 for x in test_sizes]
     training_accuracies = [plot_data[x][0] for x in plot_data]
@@ -320,33 +337,60 @@ def pines_classification(R_filepath, run_pca=False, run_lda=False):
 
 def plot_classification(iris, pines):
     # Plots the training/overall accuracies of each model at each test-size
-    test_sizes = [.5, .4, .3, .2, .1][::-1]
+    test_sizes = [.9, .8, .7, .6, .5]
     if pines:
         # Saved data from PINES classification output (Hard-coded to avoid re-running models)
-        # No PCA
-        accuracies = []
-        classification_accuracies = []
-        training_accuracies = [56.51636225266362, 56.59532302814111, 56.64367737990079, 56.68995243757431, 56.72233379135398][::-1]
-        plt.scatter(test_sizes, accuracies, label="Classification accuracy",c="blue")
-        plt.scatter(test_sizes, training_accuracies, label="Training accuracy",c="red")
-        plt.xlabel("% test size")
-        plt.ylabel("Average accuracy")
-        plt.title("Supervised Classification without Dimensionality Reduction on INDIAN PINES")
-        plt.legend()
-        plt.show()
-        # After PCA
-        pca_accuracies = [61.07914011224199, 61.278240190249704, 61.711318960050725, 61.86682520808561, 62.684260580123635][::-1]
-        training_accuracies = [63.5773401826484, 63.77724930638129, 64.07555887748861, 64.19887039239001, 64.3444139097347][::-1]
-        plt.scatter(test_sizes, pca_accuracies, label="Classification accuracy")
-        plt.scatter(test_sizes, training_accuracies, label="Training accuracy")
-        plt.xlabel("% test size")
-        plt.ylabel("Average accuracy")
-        plt.title("Supervised Classification with PCA Reduction on INDIAN PINES")
-        plt.legend()
-        plt.show()
+        # No reduction, PCA, LDA
+        classification_accuracies = [[[45.018970189701896, 70.47154471544715, 34.99186991869919, 46.38482384823848], [46.52439024390244, 74.78048780487805, 35.13414634146341, 46.96341463414634], [46.99651567944251, 76.2787456445993, 35.83275261324042, 47.63763066202091], [46.959349593495936, 76.04878048780488, 38.40650406504065, 47.54471544715447], [46.868292682926835, 76.9560975609756, 37.44390243902439, 47.88292682926829]],
+                                     [[68.21680216802169, 64.05420054200542, 28.899728997289976, 47.2520325203252], [69.15853658536585, 67.8048780487805, 35.71951219512195, 51.0609756097561], [69.70034843205575, 69.4773519163763, 36.139372822299656, 54.67595818815331], [69.04065040650407, 69.3170731707317, 38.146341463414636, 57.05691056910569], [69.01463414634146, 69.95121951219512, 37.44390243902439, 57.11219512195122]],
+                                     [[81.97289972899729, 81.31165311653118, 38.68834688346883, 66.16802168021681], [82.9390243902439, 83.42682926829268, 49.28048780487805, 73.01219512195122], [83.94425087108014, 84.6411149825784, 56.19512195121952, 75.76306620209058], [84.01626016260163, 84.6341463414634, 59.91869918699187, 77.08943089430895], [84.0780487804878, 85.26829268292683, 60.74146341463415, 78.2439024390244]]]
+        training_accuracies = [[[0.494140625, 0.8251953125, 0.3662109375, 0.4853515625], [0.4924353343094192, 0.8306490971205466, 0.35675939482674474, 0.4719375305026842], [0.47950553025374104, 0.8487312947299935, 0.36792452830188677, 0.47918022121014964], [0.4764576726030739, 0.8497194437667723, 0.3954623078799707, 0.47962917784825565], [0.48614363778298203, 0.8489461358313818, 0.3704137392661983, 0.4781420765027322]],
+                               [[0.732421875, 0.7822265625, 0.3095703125, 0.4912109375], [0.7101024890190337, 0.7823328452903856, 0.36505612493899464, 0.5148853099072719], [0.7029928432010409, 0.8061158100195186, 0.36890045543266103, 0.5487963565387117], [0.708709441327153, 0.797511588192242, 0.3827762868992437, 0.5708709441327153], [0.7008196721311475, 0.8093286494925839, 0.3620218579234973, 0.5669398907103825]],
+                               [[0.849609375, 0.8828125, 0.400390625, 0.671875], [0.8355295265983407, 0.8867740361151781, 0.4987798926305515, 0.72132747681796], [0.8308392973324659, 0.8916720884840599, 0.5676642810670136, 0.7514638906961614], [0.8311783361795559, 0.8968040985606246, 0.5964869480361064, 0.771895584288851], [0.8370413739266198, 0.9024199843871975, 0.6065573770491803, 0.7790788446526151]]]
+        for i, accuracies in \
+                enumerate(zip(classification_accuracies, training_accuracies)): 
+            classification_accuracies_itr = accuracies[0]
+            training_accuracies_itr = accuracies[1]
+            KNN_accuracies = [x[0] for x in classification_accuracies_itr]
+            KNN_train_accuracies = [x[0] for x in training_accuracies_itr]
+            SVM_poly_accuracies = [x[1] for x in classification_accuracies_itr]
+            SVM_poly_train_accuracies = [x[1] for x in training_accuracies_itr]
+            SVM_rbf_accuracies = [x[2] for x in classification_accuracies_itr]
+            SVM_rbf_train_accuracies = [x[2] for x in training_accuracies_itr]
+            GNB_accuracies = [x[3] for x in classification_accuracies_itr]
+            GNB_train_accuracies = [x[3] for x in training_accuracies_itr]
+            plt.plot(test_sizes, KNN_accuracies, label="KNN Classification Accuracies")
+            plt.plot(test_sizes, SVM_poly_accuracies,label="SVM-Poly Classification Accuracies")
+            plt.plot(test_sizes, SVM_rbf_accuracies,label="SVM-rbf Classification Accuracies")
+            plt.plot(test_sizes, GNB_accuracies,label="Gaussian Classification Accuracies")
+            plt.xlabel("% test size")
+            plt.ylabel("Average Classification Accuracies")
+            if i == 0:
+                plt.title("Supervised Classification without Dimensionality Reduction on PINES")
+            elif i == 1:
+                plt.title("Supervised Classification with PCA Reduction on PINES")
+            elif i == 2:
+                plt.title("Supervised Classification with LDA Reduction on PINES")
+            plt.legend()
+            plt.show()
+            plt.figure()
+            plt.plot(test_sizes, GNB_train_accuracies, label="Gaussian Training Accuracies")
+            plt.plot(test_sizes, KNN_train_accuracies, label="KNN Training Accuracies")
+            plt.plot(test_sizes, SVM_poly_train_accuracies, label="SVM-Poly Training Accuracies")
+            plt.plot(test_sizes, SVM_rbf_train_accuracies, label="SVM-rbf Training Accuracies")
+            plt.xlabel("% test size")
+            plt.ylabel("Average Training Accuracies")
+            if i == 0:
+                plt.title("Supervised Classification without Dimensionality Reduction on PINES")
+            elif i == 1:
+                plt.title("Supervised Classification with PCA Reduction on PINES")
+            elif i == 2:
+                plt.title("Supervised Classification with LDA Reduction on PINES")
+            plt.legend()
+            plt.show()
     if iris:
         # Saved data from IRIS classification output (Hard-coded to avoid re-running models)
-        # No PCA
+        # No reduction, PCA, LDA <- data order
         training_accuracies = [[[86.66666666666667, 53.333333333333336, 86.66666666666667, 100.0],
                                [96.66666666666667, 43.333333333333336, 76.66666666666667, 100.0],
                                [95.55555555555556, 53.333333333333336, 91.11111111111111, 95.55555555555556],
@@ -377,9 +421,11 @@ def plot_classification(iris, pines):
                                       [97.14285714285714, 63.8095238095238, 96.19047619047619, 99.04761904761905],
                                       [97.77777777777777, 64.44444444444444, 93.33333333333333, 98.88888888888889],
                                       [97.33333333333334, 64.0, 94.66666666666667, 100.0]]]
-
+        # It's ugly but it works. Iterate through the data and display each plot.
+        # Order is KNN, SVM-Poly, SVM-rbf, Gaussian. This way I don't have to rerun the 
+        # classification (slow) to display data (fast)
         for i, accuracies in \
-                enumerate(zip(classification_accuracies, training_accuracies)):
+                enumerate(zip(classification_accuracies, training_accuracies)): 
             classification_accuracies_itr = accuracies[0]
             training_accuracies_itr = accuracies[1]
             KNN_accuracies = [x[0] for x in classification_accuracies_itr]
@@ -394,7 +440,7 @@ def plot_classification(iris, pines):
             plt.plot(test_sizes, SVM_poly_accuracies,label="SVM-Poly Classification Accuracies")
             plt.plot(test_sizes, SVM_rbf_accuracies,label="SVM-rbf Classification Accuracies")
             plt.plot(test_sizes, GNB_accuracies,label="Gaussian Classification Accuracies")
-            plt.xlabel("% train size")
+            plt.xlabel("% test size")
             plt.ylabel("Average Classification Accuracies")
             if i == 0:
                 plt.title("Supervised Classification without Dimensionality Reduction on IRIS")
@@ -409,7 +455,7 @@ def plot_classification(iris, pines):
             plt.plot(test_sizes, KNN_train_accuracies, label="KNN Training Accuracies")
             plt.plot(test_sizes, SVM_poly_train_accuracies, label="SVM-Poly Training Accuracies")
             plt.plot(test_sizes, SVM_rbf_train_accuracies, label="SVM-rbf Training Accuracies")
-            plt.xlabel("% train size")
+            plt.xlabel("% test size")
             plt.ylabel("Average Training Accuracies")
             if i == 0:
                 plt.title("Supervised Classification without Dimensionality Reduction on IRIS")
@@ -429,10 +475,10 @@ def main():
     #iris_classification(run_pca=True, run_lda=False)
     #iris_classification(run_pca=False, run_lda=True)
     #iris_classification(run_pca=False, run_lda=False)
-    pines_classification(filepath, run_pca=False, run_lda=False)
-    pines_classification(filepath, run_pca=True, run_lda=False)
+    #pines_classification(filepath, run_pca=False, run_lda=False)
+    #pines_classification(filepath, run_pca=True, run_lda=False)
     pines_classification(filepath, run_pca=False, run_lda=True)
-    #plot_classification(iris=True, pines=False)
+    #plot_classification(iris=True, pines=True)
 
 
 if __name__ == "__main__":
